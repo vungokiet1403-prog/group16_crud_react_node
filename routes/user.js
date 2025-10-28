309e6459204a0d77d42106942062250ea4a3672d
vungokiet1403-prog <vungokiet1403@gmail.com>
2025-10-28 15:53:57 +0700

    Đẩy toàn bộ code backend server và route user

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,23 @@
 // routes/user.js
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
 
-// CRUD routes
-router.get("/users", userController.getUsers);
-router.post("/users", userController.createUser);
-router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
+// Test route
+router.get("/", (req, res) => {
+  res.send("User API hoạt động!");
+});
+
+// Đăng ký
+router.post("/signup", (req, res) => {
+  res.json({ message: "Đăng ký thành công" });
+});
+
+// Đăng nhập
+router.post("/login", (req, res) => {
+  res.json({ message: "Đăng nhập thành công" });
+});
+router.get("/test", (req, res) => {
+  res.send("API test OK");
+});
 
 module.exports = router;
